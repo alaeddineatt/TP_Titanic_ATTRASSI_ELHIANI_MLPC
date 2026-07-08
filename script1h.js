ce3cf2c43e843488b3aae60586a89d4366c8abe4
alaeddineatt <31952379+alaeddineatt@users.noreply.github.com>
2026-07-08 15:55:47 +0200

    ERROR

--- script1h.js
+++ script1h.js
@@ -6,4 +6,8 @@ var epochPast = epochNow - 43200;
 
 // Sauvegarde dans NeoLoad
 context.variableManager.setValue("TO_EPOCH", epochNow.toString());
+
 context.variableManager.setValue("FROM_EPOCH", epochPast.toString());
+
+2026/07/08 15:53:54 INFO  - neoload.LG_INFO: Jetty ThreadPool configuration: MinThread=20; MaxThread=20000; IdleTimeoutMs=10000; QueueSize=6000 
+2026/07/08 15:54:24 ERROR - neoload.Connection: Connection to Controller lost. Stop 
